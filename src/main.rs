957aa3cf6749710e331ece1394569926e75782d8
Mikhail Sinelnikov <msin87@yandex.ru>
2021-09-24 16:40:54 +0300

    second

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ use std::path::Path;
 use roxmltree::Document;
 
 #[derive(TemplateOnce)]
-#[template(path="editdatamenu.stpl")]
-struct EditDataMenuTemplate{
+#[template(path="editdatamenu/editdatamenu.stpl")]
+struct EditDataMenuTemplate <'a>{
     userId: i64,
     sqlIntResult: HashMap<String, i32>,
-    UPDATE: UPDATE
+    UPDATE: UPDATE,
+    sql_connection: &'a mut Conn
 }
 struct UPDATE{
     message: Message
@@ -29,30 +30,30 @@ fn main() {
     let opts = Opts::from_url(url).unwrap();
     let pool = Pool::new(opts).unwrap();
     let mut conn = pool.get_conn().unwrap();
-    let path = Path::new("templates/editdatamenu.stpl");
-    let txt = fs::read_to_string(path).expect("Can't read template");
-    println!("{}",txt);
-    let doc = match roxmltree::Document::parse(txt.as_str()) {
-        Ok(doc) => doc,
-        Err(e) => {
-            println!("{}",e);
-            panic!("Can't parse xml {:?}", e)
-        }
-    };
-    let result = conn.query_fold("SELECT COUNT(*) FROM airdata", 0,|acc,row:i32|{
-        acc+row
-    }).unwrap();
-    sqlIntResult.insert(String::from("airSmellCount"),0);
-    sqlIntResult.insert(String::from("otherCounter"),12);
-    sqlIntResult.insert(String::from("airSmokeCount"),16);
+    // let path = Path::new("../templates/editdatamenu/editdatamenu.stpl");
+    // let txt = fs::read_to_string(path).expect("Can't read template");
+    // println!("{}",txt);
+    // let doc = match roxmltree::Document::parse(txt.as_str()) {
+    //     Ok(doc) => doc,
+    //     Err(e) => {
+    //         println!("{}",e);
+    //         panic!("Can't parse xml {:?}", e)
+    //     }
+    // };
+     let result_option:Option<i16> = conn.query_first("SELECT COUNT(*) FROM airdata").unwrap();
+
+    // sqlIntResult.insert(String::from("airSmellCount"),0);
+    // sqlIntResult.insert(String::from("otherCounter"),12);
+    // sqlIntResult.insert(String::from("airSmokeCount"),16);
 
     let ctx = EditDataMenuTemplate{
         userId: 545,
         sqlIntResult,
         UPDATE: UPDATE {
             message: Message { from: User { id: 33 } }
-        }
+        },
+        sql_connection: conn.as_mut()
     };
-    println!("{:?}",result);
+    println!("{:?}",ctx.render_once().unwrap());
 
 }
