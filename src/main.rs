d3cf013efd3f85cf3a389dd869d264c6014985df
Mikhail Sinelnikov <msin87@yandex.ru>
2021-09-27 07:42:07 +0300

    third

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,110 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+
 use mysql::*;
 use mysql::prelude::*;
 use std::collections::HashMap;
 use std::fs;
 use sailfish::TemplateOnce;
 use std::path::Path;
-use roxmltree::Document;
+use roxmltree::{Document, Node};
+use reqwest::RequestBuilder;
+use crate::SendMethod::{Bypass, Edit, SendNew};
 
 #[derive(TemplateOnce)]
-#[template(path="editdatamenu/editdatamenu.stpl")]
-struct EditDataMenuTemplate <'a>{
-    userId: i64,
-    sqlIntResult: HashMap<String, i32>,
-    UPDATE: UPDATE,
-    sql_connection: &'a mut Conn
+#[template(path = "editdatamenu/editdatamenu.stpl")]
+struct EditDataMenuTemplate<'a> {
+    update: Update,
+    sql_connection: &'a mut Conn,
 }
-struct UPDATE{
-    message: Message
+
+struct Update {
+    message: Message,
 }
-struct Message{
-    from: User
+
+struct Message {
+    from: User,
 }
-struct User{
-    id: i64
+
+struct User {
+    id: i64,
 }
+
+fn get_valid_document(text: &str) -> Option<Document> {
+    match roxmltree::Document::parse(text) {
+        Ok(document) => {
+            if document.root_element().tag_name().name().eq("view") {
+                return Some(document);
+            }
+            None
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            None
+        }
+    }
+}
+
+fn is_text_tag_eq(first_node: Node, second_node: Node) -> bool {
+    let first_text = first_node.first_child().unwrap().next_siblings().find(|node| node.tag_name().name().eq("text"));
+    let second_text = second_node.first_child().unwrap().next_siblings().find(|node| node.tag_name().name().eq("text"));
+    if first_text.is_some() && second_text.is_some() {
+        first_text.unwrap().text().unwrap().eq(second_text.unwrap().text().unwrap())
+    }
+    false
+}
+
+enum SendMethod {
+    SendNew,
+    Edit,
+    Bypass,
+}
+
+fn get_text_send_method(old_node: Option<Node>, new_node: Node) -> SendMethod {
+    //Если
+    if old_node.is_some() {
+        match is_text_tag_eq(old_node.unwrap(), new_node) {
+            true => text_send_method = Bypass,
+            false => text_send_method = Edit
+        }
+    }
+    SendNew
+}
+
+fn transform_message(old_node: Option<Node>, new_node: Node, bot_token: &str) -> Option<Result<RequestBuilder>> {
+    let text_send_method: SendMethod;
+    let keyboard_send_method: SendMethod;
+
+    match text_send_method {
+        SendNew => {}
+        Edit => {}
+        Bypass => {}
+    }
+}
+
 fn main() {
-    let mut sqlIntResult = HashMap::new();
     let url = "mysql://msin87yand:Solovagem2@VH249.spaceweb.ru:3306/msin87yand";
     let opts = Opts::from_url(url).unwrap();
     let pool = Pool::new(opts).unwrap();
     let mut conn = pool.get_conn().unwrap();
-    // let path = Path::new("../templates/editdatamenu/editdatamenu.stpl");
-    // let txt = fs::read_to_string(path).expect("Can't read template");
-    // println!("{}",txt);
-    // let doc = match roxmltree::Document::parse(txt.as_str()) {
-    //     Ok(doc) => doc,
-    //     Err(e) => {
-    //         println!("{}",e);
-    //         panic!("Can't parse xml {:?}", e)
-    //     }
-    // };
-     let result_option:Option<i16> = conn.query_first("SELECT COUNT(*) FROM airdata").unwrap();
 
-    // sqlIntResult.insert(String::from("airSmellCount"),0);
-    // sqlIntResult.insert(String::from("otherCounter"),12);
-    // sqlIntResult.insert(String::from("airSmokeCount"),16);
-
-    let ctx = EditDataMenuTemplate{
-        userId: 545,
-        sqlIntResult,
-        UPDATE: UPDATE {
-            message: Message { from: User { id: 33 } }
+    let ctx = EditDataMenuTemplate {
+        update: Update {
+            message: Message { from: User { id: 1 } }
         },
-        sql_connection: conn.as_mut()
+        sql_connection: conn.as_mut(),
     };
-    println!("{:?}",ctx.render_once().unwrap());
-
+    let text: String = ctx.render_once().unwrap();
+    println!("{}", text);
+    let doc_option = get_valid_document(text.as_str());
+    if doc_option.is_none() {
+        return;
+    }
+    let doc = doc_option.unwrap();
+    let root = doc.root_element();
+    let child = root.first_child();
+    if child.is_some() {
+        for sibling in child.unwrap().next_siblings() {
+            println!("{}", sibling.tag_name().name());
+        }
+    }
 }
